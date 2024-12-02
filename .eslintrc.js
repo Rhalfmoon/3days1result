5092656b9c2f378e5ddf9aed1be988315c839e6c
Rhalfmoon <kakyo141@gmail.com>
2024-12-02 16:29:45 +0900

    FEAT : defalut setting

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended-type-checked',
+      'plugin:@typescript-eslint/stylistic-type-checked',
+      'airbnb',
+      'airbnb-typescript',
+      'prettier',
+    ],
+    env: {
+      node: true,
+      browser: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: ['./tsconfig.json'],
+    },
+    root: true,
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+    overrides: [
+      {
+        files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+        extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      },
+    ],
+  };
+
+  
